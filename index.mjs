53b41ba830705098494a937ed15bc2ffa451b85f
Jordan Harband <ljharb@gmail.com>
2024-03-03 22:55:29 -0800

    [meta] simplify `exports`; remove useless mjs

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import callBind from 'call-bind';
-
-import getPolyfill from 'parseint/polyfill';
-
-export default callBind(getPolyfill(), null);
-
-export { default as getPolyfill } from 'parseint/polyfill';
-export { default as implementation } from 'parseint/implementation';
-export { default as shim } from 'parseint/shim';
